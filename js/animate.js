e66a096dcdf55ba8e685615caad1e9f29cb4e351
maclandrol <emmanuel.noutahi@hotmail.ca>
2016-03-15 20:31:46 -0400

    Data not dded

--- js/animate.js
+++ js/animate.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $(".circular-square").animate({
-    	"height": "10%"
-     }, 400, function(){$(".circular-square").animate({
-     	height: "100%",
-     	}, 400)}
+    	"width": "10%"
+     }, 2000, function(){$(".circular-square").animate({
+     	"width": "100%",
+     	}, 2000)}
      )
 });
