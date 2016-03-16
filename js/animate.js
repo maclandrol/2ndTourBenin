ee7245511cf35492b756b7e0de305607ae0e4505
maclandrol <emmanuel.noutahi@hotmail.ca>
2016-03-15 22:14:20 -0400

    Add data source

--- js/animate.js
+++ js/animate.js
@@ -7,17 +7,17 @@ $(document).ready(function() {
      );
 
 	$(function () {
-		$(".percent").width(1);
+		$("#talon").width(1);
 		$("#zinsou").width(1);
 	});
 
 	$(function () {
 	    $("#talon").animate({
 	       width: '24.8%'
-	    }, { duration: 1500, queue: false });
+	    }, { duration: 1000, queue: false });
 	    $("#zinsou").animate({
 	       width: '28.44%'
-	    }, { duration: 1500, queue: false });
+	    }, { duration: 1000, queue: false });
 	});
 
 });
