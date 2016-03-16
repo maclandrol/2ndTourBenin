b8961f1adac5712b731a6ed0583669a79b1c5157
maclandrol <emmanuel.noutahi@hotmail.ca>
2016-03-15 21:10:02 -0400

    Data not dded

--- js/animate.js
+++ js/animate.js
@@ -4,5 +4,15 @@ $(document).ready(function() {
      }, 10, function(){$(".circular-square").animate({
      	"width": "100%",
      	}, 400)}
-     )
+     );
+
+	$(function () {
+	    $("#talon").animate({
+	       width: '24.8%'
+	    }, { duration: 200, queue: false });
+	    $("#zinsou").animate({
+	       width: '28.44%'
+	    }, { duration: 200, queue: false });
+	});
+
 });
